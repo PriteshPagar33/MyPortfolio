b73b58ace47ebe635f87a6609109bf968676b754
pranav <pranavbhoite02@gmail.com>
2024-12-22 12:24:47 +0530

    first commit

--- src/components/particles.js
+++ src/components/particles.js
@@ -1,27 +1,14 @@
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { useEffect, useMemo, useState } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+import { useEffect, useMemo } from "react";
+import { loadSlim } from "@tsparticles/slim";
 
 const ParticlesComponent = (props) => {
-
-  const [setInit] = useState(false);
-  // this should be run only once per application lifetime
+  // Run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
-    }).then(() => {
-      setInit(true);
     });
-  }, []);
+  }, []); // Add empty dependency array to ensure this runs only once
 
   const particlesLoaded = (container) => {
     console.log(container);
@@ -43,7 +30,7 @@ const ParticlesComponent = (props) => {
           },
           onHover: {
             enable: true,
-            mode: 'grab',
+            mode: "grab",
           },
         },
         modes: {
@@ -95,7 +82,7 @@ const ParticlesComponent = (props) => {
       },
       detectRetina: true,
     }),
-    [],
+    [] // Add empty dependency array to prevent recalculating `options` unnecessarily
   );
 
   return <Particles id={props.id} init={particlesLoaded} options={options} />;
